8b2e0d5e6f7a70923a11c66718881439f7160948
adrielrobles <adropere@espol.edu.ec>
2022-06-22 00:26:07 -0500

    Añadiendo librerias, archivo de analisis lexico y prueba

---
+++ ProyectoLP.py
@@ -0,0 +1,85 @@
+import ply.lex as lexico
+
+reservadas = {
+  #Palabras reservadas
+  "in":"IN",
+  "class":"CLASS",
+  "puts":"PUTS",
+  #estructura de control
+  "if":"IF",
+  "else":"ELSE",
+  "until":"UNTIL",
+  "end":"END",
+  "case":"case",
+  "elseif":"ELSEIF",
+  #tipado
+  "String":"STRING",
+  "Integer":"INTEGER",
+  "Float":"FLOAT",
+  #funciones
+  "def":"DEF",
+  "end":"END",
+  "return":"RETURN"
+  
+}
+
+tokens = ("MAS", "MENOS", "DIV", "MULTIPLICACION", "MODULO","DOBLE_IGUAL","MULTIPLICACION_IGUAL","EXPONENCIAL_IGUAL"       , "MENOR_IGUAL","NO_IGUAL","BACKS", "MENOR_QUE", "MAYOR_IGUAL",
+         "IGUAL", "PAR_I", "PAR_D",
+         "NOMBRE_VARIABLE","VALOR_ENTERO","VALOR_DECIMAL","CORCHETE_D","CORCHETE_I") + tuple(reservadas.values())
+
+#Definir expresiones regulares
+#operadores aritmeticos
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_MULTIPLICACION= r'\*'
+t_DIV= r'/'
+t_MODULO = r'%'
+t_MULTIPLICACION_IGUAL = r'\*='
+t_EXPONENCIAL_IGUAL = r'\*\*='
+t_PAR_D = r'\)'
+t_PAR_I = r'\('
+t_IGUAL = r'='
+#OPERADORES LOGICOS
+t_MENOR_QUE = r'<'
+t_MAYOR_IGUAL = r'>='
+t_DOBLE_IGUAL= r'=='
+t_MENOR_IGUAL = r'<='
+t_NO_IGUAL = r'!='
+t_ignore = " \t"
+#caractere especiales
+t_BACKS = r'\\'
+t_VALOR_ENTERO=r'[0-9]+'
+t_VALOR_DECIMAL=r'[0-9]+.[0-9]+'
+t_CORCHETE_I=r'\['
+t_CORCHETE_D=r'\]'
+
+def t_NOMBRE_VARIABLE(t):
+  r'([a-z]|_|\$|\@)[a-zA-Z0-9_]+'
+  t.type = reservadas.get(t.value,"NOMBRE_VARIABLE")
+  return t
+
+def t_contadorLineas(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print(f"Caracter no reconocido {t.value[0]} en línea {t.lineno}")
+    t.lexer.skip(1)
+  
+validador = lexico.lex()
+
+def getTokens(lexer):
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break 
+        print(tok)
+
+linea=" "
+codigo = open("source.vb")
+for linea in codigo:
+  validador.input(linea)
+  getTokens(validador)
+codigo.close()
+
+print("Analisis terminado... :)")
