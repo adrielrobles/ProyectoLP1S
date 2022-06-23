e294ec73d227fe7079aeed6dc864fd8ad0eccdfb
DarinkaTownsend <darinka.townsend@hotmail.com>
2022-06-23 12:47:39 -0500

    Trabajo Darinka

--- ProyectoLP.py
+++ ProyectoLP.py
@@ -19,16 +19,26 @@ reservadas = {
   #funciones
   "def":"DEF",
   "end":"END",
-  "return":"RETURN"
+  "return":"RETURN",
+
+  #Darinka Townsend
+  #OPERADORES DE COMPARACION
+  "and" : "AND",
+  "or" : "OR",
+  "not" : "NOT",
+  #HASHES
+  "dict" : "DICCIONARIO"
+
   
 }
 
 tokens = ("MAS", "MENOS", "DIV", "MULTIPLICACION", "MODULO","DOBLE_IGUAL","MULTIPLICACION_IGUAL","EXPONENCIAL_IGUAL"       , "MENOR_IGUAL","NO_IGUAL","BACKS", "MENOR_QUE", "MAYOR_IGUAL",
          "IGUAL", "PAR_I", "PAR_D",
-         "NOMBRE_VARIABLE","VALOR_ENTERO","VALOR_DECIMAL","CORCHETE_D","CORCHETE_I") + tuple(reservadas.values())
+         "NOMBRE_VARIABLE","VALOR_ENTERO","VALOR_DECIMAL","CORCHETE_D","CORCHETE_I","ASIGNACION","t_LLAVE_I","t_LLAVE_D") + tuple(reservadas.values())
 
 #Definir expresiones regulares
-#operadores aritmeticos
+#Darinka Townsend
+#OPERADORES ARITMETICOS
 t_MAS = r'\+'
 t_MENOS = r'-'
 t_MULTIPLICACION= r'\*'
@@ -39,6 +49,15 @@ t_EXPONENCIAL_IGUAL = r'\*\*='
 t_PAR_D = r'\)'
 t_PAR_I = r'\('
 t_IGUAL = r'='
+
+#HASHES
+t_ASIGNACION = r'=>'
+t_LLAVE_I = r'\{'
+t_LLAVE_D = r'\}'
+
+
+
+
 #OPERADORES LOGICOS
 t_MENOR_QUE = r'<'
 t_MAYOR_IGUAL = r'>='
@@ -46,6 +65,8 @@ t_DOBLE_IGUAL= r'=='
 t_MENOR_IGUAL = r'<='
 t_NO_IGUAL = r'!='
 t_ignore = " \t"
+
+
 #caractere especiales
 t_BACKS = r'\\'
 t_VALOR_ENTERO=r'[0-9]+'
@@ -53,10 +74,13 @@ t_VALOR_DECIMAL=r'[0-9]+.[0-9]+'
 t_CORCHETE_I=r'\['
 t_CORCHETE_D=r'\]'
 
+#Darinka Townsend
 def t_NOMBRE_VARIABLE(t):
   r'([a-z]|_|\$|\@)[a-zA-Z0-9_]+'
   t.type = reservadas.get(t.value,"NOMBRE_VARIABLE")
   return t
+
+
 
 def t_contadorLineas(t):
     r'\n+'
